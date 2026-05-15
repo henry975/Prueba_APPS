78cddd86078631f18aebb47a9af0bda9e6899a21
henry975 <hendryx29032005@gmail.com>
2026-05-14 19:26:27 -0500

    ...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonItem, IonLabel, IonInput, IonToggle,
+  IonList
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    FormsModule,
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonItem, IonLabel, IonInput, IonToggle,
+    IonList
+  ],
 })
 export class HomePage {
+  nombreUsuario: string = 'Adrian y DIEGO'; 
+  colorDinamico: string = 'primary';
+  contador: number = 0;
+  textoDinamico: string = '';
+  mostrarContenido: boolean = false;
+
+  herramientas: string[] = [
+    'Ionic Framework',
+    'Angular Standalone',
+    'TypeScript',
+    'Tinkercad',
+    'Arduino'
+  ];
+
   constructor() {}
+
+  aumentarNumero() {
+    this.contador = this.contador + 1;
+  }
 }
